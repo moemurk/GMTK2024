b83d0acf850905e20a90bcd0ba3b96ae4ee83714
moemurk <murk1011@gmail.com>
2024-08-18 21:15:48 +0900

    Character & widget
    1. Character move added
    2. Button widget added

---
+++ Scripts/Player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if Input.is_action_just_pressed("left_mouse_click"):
+		show_popup_pheromones()
+	if Input.is_action_just_pressed("right_mouse_click"):
+		pass
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+	
